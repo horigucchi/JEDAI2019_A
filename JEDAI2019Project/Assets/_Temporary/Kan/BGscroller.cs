687d530d0cf8e7161d7a8b56ea2925e637890b48
smartwgy <smartwgy@gmail.com>
2019-12-16 20:44:51 +0900

    背景スクロール

---
+++ JEDAI2019Project/Assets/_Temporary/Kan/BGscroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kan
+{
+    public class BGscroller : MonoBehaviour
+    {
+
+        private Vector3 startPosition;
+
+        public float scrollSpeed;
+        private float tileSizeX;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            tileSizeX = transform.localScale.x;
+            startPosition = transform.position;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeX);
+            transform.position = startPosition + Vector3.left * newPosition;
+        }
+    }
+
+}
