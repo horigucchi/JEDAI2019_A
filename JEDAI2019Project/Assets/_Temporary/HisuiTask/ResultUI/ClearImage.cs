2133518d3b122919289e3490c1135351f2d851c2
smartwgy <smartwgy@gmail.com>
2019-12-20 09:08:52 +0900

    各UIを統合する

--- JEDAI2019Project/Assets/_Temporary/HisuiTask/ResultUI/ClearImage.cs
+++ JEDAI2019Project/Assets/_Temporary/HisuiTask/ResultUI/ClearImage.cs
@@ -7,22 +7,34 @@ using UnityEngine.UI;
 public class ClearImage : MonoBehaviour
 {
     [SerializeField]
+    private GameObject newScore;
+    [SerializeField]
     private List<GameObject> clearImages;
 
     private bool afterFlag = false;
-
+    public int highScore = 0;
+    [SerializeField]
+    private int nowScore = 0;
     private float flame;
     private Vector3 pos;
+    private bool isHighScore;
     private const int subPosX = 15;
 
     private void OnEnable()
     {
+        isHighScore = false;
         for (int i = 0; i < transform.childCount; ++i) 
         {
             clearImages.Add(transform.GetChild(i).gameObject);
         }
         pos = transform.position;
         transform.position = new Vector3(pos.x - subPosX, pos.y, pos.z);
+        highScore = HighScoreDataIO.Instance.HighScoreLoad("/Score/HighScore.txt");
+        if (highScore < (nowScore = ScoreController.Instance.GetScore()))
+        {
+            isHighScore = true;
+            HighScoreDataIO.Instance.HighScoreSave("/Score/HighScore.txt", nowScore);
+        }
     }
 
     void Update()
@@ -51,6 +63,13 @@ public class ClearImage : MonoBehaviour
         if (transform.position.x < pos.x)
         {
             transform.position += new Vector3(6 * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            if((isHighScore)&&(newScore!=null))
+            {
+                newScore.SetActive(true);
+            }
         }
     }
 
